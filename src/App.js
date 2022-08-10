f1790c161ba139da4d91c58e7248b0f503256eac
Denys Kurylenko <lightcom2001@gmail.com>
2022-08-10 16:57:36 +0300

    changed handleTouch

--- src/App.js
+++ src/App.js
@@ -5,23 +5,19 @@ import './app.scss';
 function App() {
 	const [touches, setTouches] = useState([]);
 
-	const hanldeTouchStart = e => {
-		setTouches([...e.touches]);
-	};
-	const hanldeTouchEnd = e => {
-		setTouches([...e.touches]);
-	};
-	const hanldeTouchMove = e => {
+	const hanldeTouch = e => {
 		setTouches([...e.touches]);
 	};
 
 	return (
 		<div
 			className='app'
-			onTouchStart={hanldeTouchStart}
-			onTouchEnd={hanldeTouchEnd}
-			onTouchMove={hanldeTouchMove}>
-			<h1 className={`title${touches.length > 0 ? ' fade-out' : ''}`}>Touch the screen</h1>
+			onTouchStart={hanldeTouch}
+			onTouchEnd={hanldeTouch}
+			onTouchMove={hanldeTouch}>
+			<h1 className={`title${touches.length > 0 ? ' fade-out' : ''}`}>
+				Touch the screen
+			</h1>
 			{touches.length > 0 &&
 				touches.map(touch => <Circle touch={touch} key={touch.identifier} />)}
 		</div>
