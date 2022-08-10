321b93cfbe6510ec04965c5fa9f7e0f345402e3c
Denys Kurylenko <lightcom2001@gmail.com>
2022-08-10 14:05:22 +0300

    upload test version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import Circle from './components/Circle';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [touches, setTouches] = useState([]);
+
+
+	const hanldeTouchStart = e => {
+		console.log('e.touches: ', e.touches);
+		setTouches([...e.touches]);
+	};
+	const hanldeTouchEnd = e => {
+		setTouches([...e.touches]);
+	};
+	const hanldeTouchMove = e => {
+		setTouches([...e.touches]);
+
+	};
+
+	return (
+		<div
+			className='app'
+			onTouchStart={hanldeTouchStart}
+			onTouchEnd={hanldeTouchEnd}
+			onTouchMove={hanldeTouchMove}>
+			<h1>Hello v - 1</h1>
+			{touches.length > 0 && touches.map(touch => (
+				<Circle touch={touch} key={touch.identifier}/>
+			))}
+		</div>
+	);
 }
 
 export default App;
