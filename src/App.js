f4a51ee596aa0650ad08b803ce84e530b9f3b579
Denys Kurylenko <lightcom2001@gmail.com>
2022-08-11 10:47:21 +0300

    choosing winner v3

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './app.scss';
 
 function App() {
 	const [touches, setTouches] = useState([]);
+	const [winner, setWinner] = useState(null);
 
 	const handleTouch = e => {
 		setTouches([...e.touches]);
@@ -25,24 +26,14 @@ function App() {
 
 	useEffect(() => {
 		const chooseWinner = () => {
-			window.removeEventListener('touchstart', handleTouch);
-			window.removeEventListener('touchend', handleTouch);
-			window.removeEventListener('touchcancel', handleTouch);
-			window.removeEventListener('touchmove', handleTouch);
-
 			const numOfTouches = touches.length;
-			const winner =
-				touches[Math.floor(Math.random() * numOfTouches)].identifier;
+			const winner = touches[Math.floor(Math.random() * numOfTouches)];
 			console.log('winner: ', winner);
-			setTouches(prevTouches =>
-				prevTouches.filter(t => t.identifier === winner)
-			);
+			setWinner(winner);
 
 			setTimeout(() => {
-				window.addEventListener('touchstart', handleTouch);
-				window.addEventListener('touchend', handleTouch);
-				window.addEventListener('touchcancel', handleTouch);
-				window.addEventListener('touchmove', handleTouch);
+				console.log('timeout winner');
+				setWinner(null);
 			}, 2000);
 		};
 
@@ -50,6 +41,7 @@ function App() {
 
 		if (touches.length > 1) {
 			clearTimeout(timer);
+			console.log('clear timeout');
 
 			timer = setTimeout(() => {
 				chooseWinner();
@@ -68,7 +60,10 @@ function App() {
 				Touch the screen
 			</h1>
 			{touches.length > 0 &&
+				winner !== null &&
 				touches.map(touch => <Circle touch={touch} key={touch.identifier} />)}
+
+			{winner && <Circle touch={winner} key={winner.identifier} />}
 		</div>
 	);
 }
