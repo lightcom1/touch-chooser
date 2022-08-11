4c41b85925ecf56365b98280fe66a2ddbbea3704
Denys Kurylenko <lightcom2001@gmail.com>
2022-08-11 12:18:11 +0300

    fixed circle styles

--- src/components/Circle.jsx
+++ src/components/Circle.jsx
@@ -1,10 +1,11 @@
 import './circle.scss';
 
 const Circle = ({ touch }) => {
+	console.log('touch: ', touch);
 
 	return (
 		<div
-			className={`circle${touch?.clientX ? '' : ' hide'}`}
+			className={`circle${touch ? '' : ' hide'}`}
 			style={{ left: touch?.clientX, top: touch?.clientY }}></div>
 	);
 };
